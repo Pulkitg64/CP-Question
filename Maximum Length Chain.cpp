ce0026fc46854dcce7bac282f99fc9ed0ecd5385
Pulkit Gupta <pulkitgupta64@gmail.com>
2020-07-22 16:36:35 +0530

    Max Length Chain

---
+++ Maximum Length Chain.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	int * arr = new int[10001];
+	arr[0] = 0;
+	arr[1] = 1;
+	arr[2] = 2;
+	
+	for(int i=3;i<10001;i++){
+	    int x = arr[i-1];
+	    if(i%2==0){
+	        x = min(x,arr[i/2]);
+	    }
+	    arr[i] = x+1;
+	}
+	while(t--){
+	    int n;
+	    cin>>n;
+	    cout<<arr[n]<<endl;
+	}
+	
+	
+	return 0;
+}
